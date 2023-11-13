f940a7d87d5353de632c42502a5b4a1db0c486c1
abdelrhman mohamed <fahmy@gmail.com>
2023-11-13 10:34:36 +0200

    add service image in product system

---
+++ app/Services/FileService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Services;
+
+
+class FileService {
+
+    public function storeFile($file)
+    {
+        try{
+            $fileName = time().rand(1,99).'.'.$file->extension();
+            $file->move(public_path('uploads'), $fileName);
+            return $fileName;
+        }catch(\Exception $e){
+            return $e;
+        }
+    }
+
+    public function updateFile($file , $product)
+    {
+        try{
+            if($product->image == null){
+                $fileName = time().rand(1,99).'.'.$file->extension();
+                $file->move(public_path('uploads'), $fileName);
+                return $fileName;
+            }else{
+                if($product){
+                    unlink("uploads/".$product->image);
+                }
+                $fileName = time().rand(1,99).'.'.$file->extension();
+                $file->move(public_path('uploads'), $fileName);
+                return $fileName;
+            }
+            
+        }catch(\Exception $e){
+            return $e;
+        }
+    }
+
+    public function deleteFileFromUploads($id , $productRepository)
+    {
+        try{
+            $model = $productRepository->find($id , []);
+            if($model){
+                unlink("uploads/".$model->image);
+            }
+        }catch(\Exception $e){
+            return $e;
+        }
+    }
+
+}
