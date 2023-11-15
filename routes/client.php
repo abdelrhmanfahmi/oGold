c1b9f88e4400884f30c524e0e5a9a53cb0f11fa3
abdelrhman mohamed <fahmy@gmail.com>
2023-11-15 12:04:20 +0200

    test issue of get market watch

--- routes/client.php
+++ routes/client.php
@@ -9,7 +9,7 @@ $router->group([ 'namespace' => 'App\Http\Controllers\Client'], function () use 
 
 $router->group(['prefix' => 'products' ,'namespace' => 'App\Http\Controllers\Client'], function () use ($router) {
     $router->get('/', ['as' => 'products.index', 'uses' => 'ProductController@index']);
-    // $router->get('/market-watch', ['as' => 'products.market', 'uses' => 'ProductController@getMarketWatch']);
+    $router->get('/market-watch', ['as' => 'products.market', 'uses' => 'ProductController@getMarketWatch']);
     $router->post('/buy-gold', ['as' => 'products.buy_gold', 'uses' => 'ProductController@buyGold']);
     $router->post('/sell-gold', ['as' => 'products.sell_gold', 'uses' => 'ProductController@sellGold']);
 });
