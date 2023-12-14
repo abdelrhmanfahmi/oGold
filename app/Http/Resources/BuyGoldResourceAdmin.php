beceab00c34bdbf1f2ea7c1ad7fb03b4843b57b3
aitsystemsgit <abdo.fahmy9696@gmail.com>
2023-12-14 20:39:05 +0200

    add routes for refinery order index and for admin methods & fix issue in flow back

--- app/Http/Resources/BuyGoldResourceAdmin.php
+++ app/Http/Resources/BuyGoldResourceAdmin.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class OrderDeliveryResource extends JsonResource
+class BuyGoldResourceAdmin extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -14,8 +14,6 @@ class OrderDeliveryResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
-            'products' => ProductResource::collection($this->whenLoaded('products'))
-        ];
+        return parent::toArray($request);
     }
 }
