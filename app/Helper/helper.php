24a36d4e2aa6b57b97b1583c0f50fdf31c784b15
aitsystemsgit <abdo.fahmy9696@gmail.com>
2024-01-09 23:58:46 +0200

    fix issue in flow back

--- app/Helper/helper.php
+++ app/Helper/helper.php
@@ -1,2 +1,23 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+
+function getBuyPrice()
+{
+    try{
+        $client = new \GuzzleHttp\Client();
+        $url = 'https://platform.ogold.app/mtr-api/'.env('SYSTEMUUID').'/quotations?symbols=GoldGram24c&applyMarkup=true';
+        $response = $client->request('GET', $url, [
+            'headers' => [
+                'co-auth' => Auth::user()->co_auth,
+                'Auth-trading-api' => Auth::user()->trading_api_token,
+                'Cookie' => 'co-auth='. Auth::user()->co_auth
+            ],
+        ]);
+        $result = $response->getBody()->getContents();
+        $decodedData = json_decode($result);
+        return $decodedData;
+    }catch(\Exception $e){
+        return $e;
+    }
+}
--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -19,6 +19,7 @@ class ProductResource extends JsonResource
             'name' => $this->name ?? null,
             'gram' => $this->gram ?? null,
             'image' => $this->when($this->image,env('APP_URL') .'/uploads/' . $this->image,null),
+            'quantity' => $this->pivot->quantity ?? null
             // 'is_active' => $this->is_active ?? null
             // 'orders' => OrderResource::collection($this->whenLoaded('orders'))
         ];
