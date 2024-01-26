f6d2b4ef1afb92a1b69148f13ca6267c8e1682bc
aitsystemsgit <abdo.fahmy9696@gmail.com>
2024-01-26 15:50:37 +0200

    fix issue in flow cro job catalog

--- app/Console/Commands/UpdatePriceCommand.php
+++ app/Console/Commands/UpdatePriceCommand.php
@@ -26,31 +26,36 @@ class UpdatePriceCommand extends Command
         $user = User::where('email' , env('EMAILUPDATEPRICE'))->first();
         $buyPrice = $this->matchService->getMarketWatchSymbolPerUser($user->id);
         $catalogs = Catalog::with('products')->get();
-        if(!is_string($buyPrice)){
-            foreach($catalogs as $catalog){
-                foreach($catalog->products as $product){
-                    $totalPrice = ($buyPrice[0]->ask * $product->gram) + $product->charge + ($catalog->preimum_fees * $product->gram);
-                    DB::table('catalog_products')
-                    ->where('catalog_id' , $catalog->id)
-                    ->where('product_id' , $product->id)
-                    ->update(['total_price' => $totalPrice]);
+        if(count($catalogs) > 0){
+            if(!is_string($buyPrice)){
+                foreach($catalogs as $catalog){
+                    foreach($catalog->products as $product){
+                        $totalPrice = ($buyPrice[0]->ask * $product->gram) + $product->charge + ($catalog->preimum_fees * $product->gram);
+                        DB::table('catalog_products')
+                        ->where('catalog_id' , $catalog->id)
+                        ->where('product_id' , $product->id)
+                        ->update(['total_price' => $totalPrice]);
+                    }
                 }
+                \Log::info('work fine one');
+            }else{
+                //login with user
+                $this->matchService->loginAccountForCronJob();
+                $buyPrice = $this->matchService->getMarketWatchSymbolPerUser($user->id);
+                foreach($catalogs as $catalog){
+                    foreach($catalog->products as $product){
+                        $totalPrice = ($buyPrice[0]->ask * $product->gram) + $product->charge + ($catalog->preimum_fees * $product->gram);
+                        DB::table('catalog_products')
+                        ->where('catalog_id' , $catalog->id)
+                        ->where('product_id' , $product->id)
+                        ->update(['total_price' => $totalPrice]);
+                    }
+                }
+                \Log::info('work fine two');
             }
-            \Log::info('work fine one');
         }else{
-            //login with user
-            $this->matchService->loginAccountForCronJob();
-            $buyPrice = $this->matchService->getMarketWatchSymbolPerUser($user->id);
-            foreach($catalogs as $catalog){
-                foreach($catalog->products as $product){
-                    $totalPrice = ($buyPrice[0]->ask * $product->gram) + $product->charge + ($catalog->preimum_fees * $product->gram);
-                    DB::table('catalog_products')
-                    ->where('catalog_id' , $catalog->id)
-                    ->where('product_id' , $product->id)
-                    ->update(['total_price' => $totalPrice]);
-                }
-            }
-            \Log::info('work fine two');
+            \Log::info('no catalogs yet');
         }
+
     }
 }
