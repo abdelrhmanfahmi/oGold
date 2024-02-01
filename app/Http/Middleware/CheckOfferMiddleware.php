46dac0ead1c57ce8673f4cfeca2be42cbda313ad
Sayed Gamal <sayed@fabrica.com.eg>
2024-02-01 22:27:51 +0200

    debug

--- app/Http/Middleware/CheckOfferMiddleware.php
+++ app/Http/Middleware/CheckOfferMiddleware.php
@@ -5,7 +5,6 @@ namespace App\Http\Middleware;
 use App\Models\Offers;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class CheckOfferMiddleware
@@ -20,11 +19,8 @@ class CheckOfferMiddleware
         //check if request url login and user is admin or refinery
         if(str_contains($request->url(), 'api/auth/admin/login')){
             return $next($request);
-        }elseif(str_contains($request->url(), 'api/auth/user/info')){
-            return $next($request);
         }else{
-//            dd($request->header(``'offer_id'));
-            $offer = $request->header('offer');
+            $offer = $request->headers->get('offer');
             if($offer){
                 $checkOffer = Offers::where('offer_id' , $offer)->exists();
                 if(!$checkOffer){
